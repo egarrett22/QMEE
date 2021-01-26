d8d0fb02677e5cac76fe903c00c66b4776485f5f
egarrett22 <garrett.emily22@gmail.ca>
2021-01-26 11:32:26 -0500

    Jan26

--- QMEE assignment Jan 22.R
+++ QMEE assignment Jan 22.R
@@ -1,9 +1,8 @@
+mean(Trial_data_QMEE_assignment_Jan_22$A750)
 library(tidyverse)
-
-Trial_data_QMEE_assignment_Jan_22 <- read_csv("Trial data QMEE assignment Jan 22.csv")
-
-
+df=Trial_data_QMEE_assignment_Jan_22
+names(Trial_data_QMEE_assignment_Jan_22)=c("protein_concentration", "A750")
+df_name_means=df%>%group_by(protein)%>%dplyr::summarise(Mean=means("protein_concentration", na.rm=TRUE))
 means=Trial_data_QMEE_assignment_Jan_22%>%
-  group_by(Protein)%>%
+  group_by(protein_concentration)%>%
   dplyr::summarise(Mean=mean(A750))
-print(means)
