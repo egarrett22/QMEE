dfc2ea749012e44d3c90ebab98658a01001682c5
egarrett22 <garrett.emily22@gmail.ca>
2021-01-26 11:27:53 -0500

    Changes from Jan 26th

--- QMEE assignment Jan 22.R
+++ QMEE assignment Jan 22.R
@@ -1,8 +1,9 @@
-mean(Trial_data_QMEE_assignment_Jan_22$A750)
 library(tidyverse)
-df=Trial_data_QMEE_assignment_Jan_22
-names(Trial_data_QMEE_assignment_Jan_22)=c("protein_concentration", "A750")
-df_name_means=df%>%group_by(protein)%>%dplyr::summarise(Mean=means("protein_concentration", na.rm=TRUE))
+
+Trial_data_QMEE_assignment_Jan_22 <- read_csv("Trial data QMEE assignment Jan 22.csv")
+
+
 means=Trial_data_QMEE_assignment_Jan_22%>%
-  group_by(protein_concentration)%>%
+  group_by(Protein)%>%
   dplyr::summarise(Mean=mean(A750))
+print(means)
