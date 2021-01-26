654a9ac732fe7e16aac04a77e915d3ff5269a707
egarrett22 <garrett.emily22@gmail.ca>
2021-01-26 11:31:07 -0500

    Updated version of the Jan 22 assignment

---
+++ QMEEJan26.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+Trial_data_QMEE_assignment_Jan_22 <- read_csv("Trial data QMEE assignment Jan 22.csv")
+
+
+means=Trial_data_QMEE_assignment_Jan_22%>%
+  group_by(Protein)%>%
+  dplyr::summarise(Mean=mean(A750))
+print(means)
+     
