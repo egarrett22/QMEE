22d561397b9061f3196f0fd8b6799b910461bde8
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-01-27 11:58:54 -0500

    performance reboot

--- Jan22code.R
+++ Jan22code.R
@@ -1,24 +1,12 @@
-## I hope it's OK that I moved your files (names should not have spaces, and don't need to repeat the fact that this is QMEE, since that's the repo name)
-## I don't understand this code at all.
-## You don't have any code that would read the file into R (I know I didn't fix the file name, but neither did you, and none of this would read any sort of file).
-## Remember, you should be able to "restart" R and "source" your code.
-## I am marking this "incomplete" -- JD
-## There is also no explanation that I can see in README.
-mean(Trial_data_QMEE_assignment_Jan_22$A750)
-library(tidyverse)
-df=Trial_data_QMEE_assignment_Jan_22
-names(Trial_data_QMEE_assignment_Jan_22)=c("protein_concentration", "A750")
-df_name_means=df%>%group_by(protein)%>%dplyr::summarise(Mean=means("protein_concentration", na.rm=TRUE))
-means=Trial_data_QMEE_assignment_Jan_22%>%
-  group_by(protein_concentration)%>%
-  dplyr::summarise(Mean=mean(A750))
-
 ##updates to the Jan22 assignment for class. From this line down the code should run
+## JD: The goal is that code should just run; I've deleted what's above here
+## You can use github to save parts of it if that proves necessary
+## You're also free to edit and track files that aren't part of your assignment
 
 library(tidyverse)
 
-Trial_data_QMEE_assignment_Jan_22 <- read_csv("Trial data QMEE assignment Jan 22.csv")
-
+## JD: How strongly do you object to the shorter and simpler name?
+Trial_data_QMEE_assignment_Jan_22 <- read_csv("Jan22data.csv")
 
 means=Trial_data_QMEE_assignment_Jan_22%>%
   group_by(Protein)%>%
