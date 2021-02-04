1ab550ef9468e4363b162a7c4e601516c682c563
egarrett22 <garrett.emily22@gmail.ca>
2021-02-03 20:54:28 -0500

    feb3

--- Feb4script.R
+++ Feb4script.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(ggplot2)
 library(readr)
 
 read_csv("Feb4data.csv")
@@ -15,4 +14,9 @@ y<-Feb4data$`% change`
 
 plot.default(Feb4data)
 
+library(ggplot2)
 
+##when i run the boxplot like we did it class, i am getting these errors and have no idea how to resolve them
+box1<-(ggplot(Feb4data, aes(x=Condition,y=% change,
+                            colour=Condition
+       + geom_boxplot(outlier.colour=NULL))
