8c7bb7b2e10d2d94f53fb72ed62403f383e901cf
Ben Bolker <bbolker@gmail.com>
2021-02-14 20:44:51 -0500

    BMB comments on assignment 4

--- Feb4script.R
+++ Feb4script.R
@@ -46,3 +46,28 @@ box1 <- ggplot(data = Feb4data, aes(x=as.factor(Condition), y=Change, fill = Con
 print(box1)
 
 ## Grade 2.0/3
+
+## BMB:
+labs <- c("breathing_air", "apnea_coldwater",
+          "apnea_air","snorkel_air","snorkel_water",
+          "apnea_water")
+ff <- Feb4data %>%
+    mutate(across(Condition,
+           ~factor(.,levels=1:6,
+                   labels=labs))) %>%
+    separate(Condition, into=c("breathtype","environment"))
+
+
+## https://stackoverflow.com/questions/52122977/consistent-width-of-boxplots-if-missing-data-by-group
+theme_set(theme_bw())
+ggplot(ff, aes(x=environment, y=Change, fill = breathtype)) + 
+    geom_boxplot(alpha=0.5,
+                 notch=TRUE,
+                 position=position_dodge(preserve="single")) +
+    ## I would add points but positioning them is a pain
+    ## geom_point(aes(colour=breathtype),position=position_dodge(width=0.75)) +
+    xlab("Condition") +
+    ylab("% Change")
+
+
+
